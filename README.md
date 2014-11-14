bd07c7b835cfe0db2a7071ade3f49ddad233f10d
duivvv <geoffrey.dhuyvetters@gmail.com>
2014-11-15 00:07:41 +0100

    0.1.1 version

---
+++ README.md
@@ -0,0 +1,187 @@
+[![npm version](http://img.shields.io/npm/v/instagram-gif.svg?style=flat)](https://www.npmjs.org/package/instagram-gif)
+
+# instagram-gif
+
+Search Instagram and create a GIF with the results
+
+## install
+
+With [npm](http://npmjs.org) do:
+
+```
+npm install --save instagram-gif
+```
+
+### Example
+
+```javascript
+
+var InstagramGIF = require("../index.js");
+
+var instagram_gif = new InstagramGIF({
+	access_token: <access_token>,
+	client_id: <client_id>,
+	client_secret: <client_secret>
+});
+
+instagram_gif.create({
+	path: "./summer.gif",
+	tag: "summer",
+	delay: 200,
+	count: 10
+}, handler);
+
+function handler(err, result){
+	if(err){
+		console.log(err.msg);
+		return;
+	}
+	console.log(result.path);
+}
+```
+
+## Parameters
+
+### path [required]
+
+path where GIF file should be saved
+
+
+```javascript
+
+// ...
+
+instagram_gif.create({
+	path: "./duivvv.gif",
+	username: "duivvv"
+}, handler);
+
+```
+
+    Type: `String`
+
+### username [optional]
+
+which username to return media, filter on
+
+```javascript
+
+// ...
+
+instagram_gif.create({
+	path: "./duivvv.gif",
+	username: "duivvv"
+}, handler);
+
+```
+
+    Type: `String`
+
+### tag [optional]
+
+search media for a tag,
+
+```javascript
+
+// ...
+
+instagram_gif.create({
+	path: "./summer.gif",
+	tag: "summer"
+}, handler);
+
+```
+
+add username for extra filter on tag
+
+```javascript
+
+// ...
+
+instagram_gif.create({
+	path: "./summer.gif",
+	tag: "summer",
+	username: "duivvv"
+}, handler);
+
+```
+
+### count [optional]
+
+amount of pictures to load (can return less if filtering is applied)
+
+```javascript
+
+// ...
+
+instagram_gif.create({
+	path: "./duivvv.gif",
+	username: "duivvv",
+	count: 10
+}, handler);
+
+```
+
+    Type: `Number`
+    default: 300
+
+### repeat [optional]
+
+frame to start repeat after end
+
+```javascript
+
+// ...
+
+instagram_gif.create({
+	path: "./duivvv.gif",
+	username: "duivvv",
+	repeat: 2
+}, handler);
+
+```
+
+    Type: `Number`
+    default: 0
+
+### delay [optional]
+
+delay between each frame
+
+```javascript
+
+// ...
+
+instagram_gif.create({
+	path: "./duivvv.gif",
+	username: "duivvv",
+	delay: 100
+}, handler);
+
+```
+
+    Type: `Number`
+    default: 300
+
+### quality [optional]
+
+frame quality
+
+```javascript
+
+// ...
+
+instagram_gif.create({
+	path: "./duivvv.gif",
+	username: "duivvv",
+	quality: 3
+}, handler);
+
+```
+
+    Type: `Number`
+    default: 10
+
+## License
+
+[MIT License](http://en.wikipedia.org/wiki/MIT_License)
---
+++ index.js
@@ -0,0 +1,161 @@
+var request = require('request');
+var api = require('instagram-node').instagram();
+var Canvas = require('canvas');
+var Image = Canvas.Image;
+var _ = require('lodash');
+var GIFEncoder = require('gifencoder');
+var Imagemin = require('imagemin');
+
+var fs = require('fs');
+
+function InstagramGIF(config){
+	if(!config.access_token){
+		throw new Error("please provide an access_token");
+	}
+	if(!config.client_id){
+		throw new Error("please provide a client_id");
+	}
+	if(!config.client_secret){
+		throw new Error("please provide a client_secret");
+	}
+	api.use({
+		access_token: config.access_token
+	});
+	api.use({
+		client_id: config.client_id,
+		client_secret: config.client_secret
+	});
+}
+
+function _init_encoder(params, width, height){
+
+	var encoder = new GIFEncoder(width, height);
+	encoder.createReadStream().pipe(fs.createWriteStream(params.path));
+
+	encoder.start();
+	encoder.setRepeat(params.repeat);
+	encoder.setDelay(params.delay);
+	encoder.setQuality(params.quality);
+
+	return encoder;
+}
+
+function _init_canvas(width, height){
+	var canvas = new Canvas(width, height);
+	var ctx = canvas.getContext('2d');
+	return ctx;
+}
+
+function _handle_results(params, results, cb){
+
+	this.width = results[0].images.standard_resolution.width;
+	this.height = results[0].images.standard_resolution.height;
+
+	this.converted = 0;
+
+	this.encoder = _init_encoder(params, this.width, this.height);
+	this.ctx =  _init_canvas(this.width, this.height);
+
+	if(params.tag && params.username){
+		results = _.filter(results, function(result){
+			return result.user.username === params.username;
+		});
+	}
+
+	for(var i = 0;i < results.length;i++){
+
+			var url = results[i].images.standard_resolution.url;
+
+			request({url: url, encoding: null}, (function (error, response, body) {
+
+				var img = new Canvas.Image();
+
+				img.onload = (function(){
+					this.converted ++;
+					this.ctx.drawImage(img, 0, 0);
+					this.encoder.addFrame(this.ctx);
+					if(results.length === this.converted){
+						this.encoder.finish();
+						var imagemin = new Imagemin().use(Imagemin.gifsicle());
+						imagemin.src(params.path);
+						imagemin.dest(".");
+						imagemin.run(function(err, files){
+					    if(err) {
+					      throw err;
+					    }
+							return cb(null, {path: params.path});
+						})
+					}
+				}).bind(this);
+
+				img.onerror = (function(){
+					console.log("error");
+				}).bind(this);
+
+				img.src = new Buffer(body, 'binary');
+				
+			}).bind(this));
+
+	}
+
+}
+
+InstagramGIF.prototype.create = function(params, cb){
+
+	params = params || {};
+	params.path = params.path || false;
+
+	if(!params.path){
+		throw new Error("please provide a path");
+	}
+
+	params.repeat = parseInt(params.repeat) || 0;
+	params.delay = parseInt(params.delay) || 300;
+	params.username = params.username || false;
+	params.count = parseInt(params.count) || 20;
+	params.tag = params.tag || false;
+	params.quality = parseInt(params.quality) || 10;
+
+	if(params.tag){
+
+		api.tag_media_recent(params.tag, {count: params.count}, (function(err, results, pagination, remaining, limit) {
+
+			if(err){
+				return cb({msg: err});
+			}
+
+			if(results && results.length > 0){
+				_handle_results.call(this, params, results, cb);
+			}
+
+		}).bind(this));
+
+	}else if(params.username){
+
+		api.user_search(params.username, function(err, users, remaining, limit){
+
+			if(err){
+				return cb({msg: err});
+			}
+
+			api.user_media_recent(users[0].id, {count: params.count}, (function(err, results, pagination, remaining, limit) {
+
+				if(err){
+					return cb({msg: err});
+				}
+
+				if(results && results.length > 0){
+					_handle_results.call(this, params, results, cb);
+				}
+
+			}).bind(this));
+
+		});
+
+
+
+	}
+
+};
+
+module.exports = InstagramGIF;
